188c1654de4e31e37978fdb1a2804b1bc070d4eb
rockwatch <rocksistor@gmail.com>
2022-10-22 17:41:16 -0400

    Setting up node environment and callback practice file

---
+++ src/callback/index.js
@@ -0,0 +1,26 @@
+//Simple example of a callback function
+
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+function calc(num1, num2, callback){ //"callback" can be any name you like
+    return callback(num1, num2); //it's important to return our callback function, if not the result yields "undefined"
+}
+
+console.log(calc(2, 2, sum));
+
+//Another example using setTimeOut
+
+setTimeout( () =>{
+    console.log("Hello Javascript!");  //using an arrow function directly inside the argument
+}, 5000);
+// Hello Javascript!
+
+setTimeout(greeting, 3000, 'Javier'); //using a callback properly in this function
+
+function greeting(name){
+    console.log(`Hello ${name}`);
+}
+
+// Hello Javier
