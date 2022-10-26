fb827be44c3b5feaf4a86c580f564cbef95cc7b9
rockwatch <rocksistor@gmail.com>
2022-10-26 16:58:53 -0400

    finished website async logic, videos display and basic layout. Installed package for deploying main.js into github pages

--- src/callback/practice.js
+++ src/callback/practice.js
@@ -9,8 +9,24 @@
   // false
   // true
 
+  function odd(value){
+    if(value % 2 == 0){
+      return false
+    }
+    else{
+      return true
+    }
+  }
+
+  function isOdd(value){
+    return console.log(odd(value));
+  }
+
+  isOdd(4); //false
+  isOdd(5); //true
+
 //2- Exclaim
-//Write a function that returns the given string with a concatenated exclamation ! at the end. We will reuse this function, so make it reusabl
+//Write a function that returns the given string with a concatenated exclamation ! at the end. We will reuse this function, so make it reusable
 
 //Function calls
 //      exclaim('Adrian');
@@ -18,6 +34,13 @@
 // Result
 //      "Adrian!"
 //      "Adrian!!"
+
+function exclaim(value){
+   return `${value}!`;
+}
+
+console.log(exclaim('Adrian')); //Adrian!
+console.log(exclaim(exclaim('Adrian')));
 
 //3- Double the chars!
 //Write a function that duplicates each char in a string. If I pass 'abc' to the function, it should return 'aabbcc'. We will reuse this function, so make it reusable.
@@ -29,6 +52,12 @@
 //   "AAddrriiaann"
 //   "ssssssssnnaakkee"
 
+function doubleChars(value){
+  const arrayed = Array(value);
+  
+  console.log(concated);
+}
+doubleChars('abc');
 
 //4- At least two elements
 // Write a function called atLeastTwo that receives an array and a callback as its arguments. If at least two elements from the array return a truthy value when passed as an argument to the callback, atLeastTwo should return true. If there aren't at least two elements from the array that return a truthy value when passed as arguments to the callback, atLeastTwo should return false.
@@ -118,7 +147,7 @@
 //     ]
 //   }
 
-// Repeat the function x times
+//6- Repeat the function x times
 // Write a repeat function that receives 3 arguments: a string, number of repetitions, and a callback that will be repeated. The repeat function should pass the string to the callback as an argument and repeat the callback x times, with the result of the previous repetition as an argument.
 
 // Function calls
